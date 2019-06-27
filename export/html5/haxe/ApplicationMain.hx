8f23a7f8ae241074f6418dabcb49afd6bf467767
IKKRIUS\Henrique <henrique.alberto.andrade@hotmail.com>
2019-06-27 06:54:43 -0300

    Final?

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "306";
+		app.meta["build"] = "308";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "Strike Tanker STT";
 		app.meta["name"] = "Strike Tanker STT";
